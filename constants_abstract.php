1e142bb73938758c88fc152d77985792a8dd6e7b
shivshankarzapbuild <shivshankar.zapbuild@gmail.com>
2020-01-16 15:29:55 +0530

    constant and abstract

--- constants_abstract.php
+++ constants_abstract.php
@@ -1,3 +1,40 @@
 <?php 
+    // Declaring class 
+    class Test {
+        //private variables
+        private $name;
+        private $batch;
+        const STRING = "This is a simple constant String ";
+        //COnstructor 
+        function __construct($name , $batch) {
+            $this->name = $name;
+            $this->batch = $batch;
+        }
+        //to return the data
+        function getName(){
+            echo "Name is {$this->name} and batch is {$this->batch}";
+
+        }
+    }
+    $test = new Test("Shiv Shankar",2020);
+    echo Test::STRING;
+    $test->getName();
+//Abstract class 
+    abstract class Test1{
+        abstract public function myFun() : string ;
+
+    }
+//Extending the abstract class
+    class Test2 extends Test1{
+       
+        //overridding the abstract function
+        public function myFun() : string{
+            return "This is just the overridden function";
+        }
+
+    }
+    $test1 = new Test2();
+    echo $test1->myFun();
+
 
 ?>
