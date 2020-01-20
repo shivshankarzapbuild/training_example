77fbdbdc3895bd8bd6415b2a1ccd03956bb29af7
shivshankarzapbuild <shivshankar.zapbuild@gmail.com>
2020-01-20 18:29:02 +0530

    in built functions

--- php_string_functions.php
+++ php_string_functions.php
@@ -26,7 +26,64 @@
 
     // converts to specified ascii value
 
-    echo chr(56);
+    echo chr(56); //decimal value conversion
+    echo chr(0x56); // hexadecial value conversion
+     // octal value conversion 
+
+     //converts into the hash value using Message Digest 5 algorithm
+     $str = "hello";
+     echo "<br>";
+     echo md5($str);
+     
+     //converts into the hash value using Secure hash algorithm
+     echo "<br>";
+     echo sha1($str);
+
+     //splits by the number of letters passed as the argument and also by the character
+     //passed in the function
+     $str = "hello world";
+     echo "<br>";
+     echo chunk_split($str,2,"#");
+
+     //Returns the information about the characters used in the string 
+     //3 is the mode of the information 
+     $str = "Hello World";
+     echo "<br>";
+     echo count_chars($str,3);
+
+     //calculates the 32-bit CRC(Cyclic Redudancy Check) checksum for the string passed
+     echo "<br>";
+     echo crc32($str);
+
+     //returns the encoded string 
+     echo "<br>";
+     echo crypt($str);
+
+     //Breaks into the array to the string passed from the gap or string passed
+
+     $str = "this is a new string for the array conversion";
+     echo "<br>";
+     print_r(explode(" ",$str));
+
+     //implode combines the array passed in the argument
+     $arr = array('Hello','World!','Beautiful','Day!');
+     echo "<br>";
+     echo implode(" ",$arr);
+
+     //join is an alias of implode i.e. does the same work of joining
+     echo "<br>";
+     echo join("+",$arr);
+
+     //this function returns ascii value of first character of the string 
+     echo "<br>";
+     echo ord($str);
+
+     //parse a string value to the variable
+     echo "<br>";
+     parse_str("name=Shivshankar&batch=2020");
+     echo $name." ".$batch;
+
+     //
 
 
     ?>
