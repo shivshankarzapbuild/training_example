631cfa9cf5af10c2d25edbd508c7182674ed162d
shivshankarzapbuild <shivshankar.zapbuild@gmail.com>
2020-01-20 18:30:18 +0530

    test file

--- test.php
+++ test.php
@@ -1,31 +1,7 @@
 <?php
-class Connection
-{
-    protected $link;
-    private $dsn, $username, $password;
-    
-    public function __construct($dsn, $username, $password)
-    {
-        $this->dsn = $dsn;
-        $this->username = $username;
-        $this->password = $password;
-        $this->connect();
-    }
-    
-    private function connect()
-    {
-        $this->link = new PDO($this->dsn, $this->username, $this->password);
-    }
-    
-    public function __sleep()
-    {
-        return array('dsn', 'username', 'password');
-    }
-    
-    public function __wakeup()
-    {
-        $this->connect();
-    }
-}
-    $fn = new Connection();
+  
+  echo mktime(0,0,0,10,13,2025);
+
+
+
 ?>
