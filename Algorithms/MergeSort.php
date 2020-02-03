6271a6f47aa5570bf819f0a0b1eb966b0566e29d
shivshankarzapbuild <shivshankar.zapbuild@gmail.com>
2020-02-03 18:32:08 +0530

    Merge sort

---
+++ Algorithms/MergeSort.php
@@ -0,0 +1,52 @@
+<?php
+
+$arr = array(12,125,451,547,56,45,457,25,174,896);
+print_r($arr);
+echo "<br>";
+$arr=mergeSort($arr);
+
+print_r($arr);
+
+function mergeSort($num)
+{
+    if(count($num)==1)
+    return $num;
+       $mid=count($num)/2;
+        //echo " $mid ";
+        $left=array_slice($num,0,$mid);
+        $right=array_slice($num,$mid);
+        $left=mergeSort($left);
+        $right=mergeSort($right);
+        return merge($left,$right);
+}
+function merge($left,$right)
+{
+    $res=array();
+    $i=0;
+    $j=0;
+    while($i<count($left) && $j<count($right))
+    {
+        if($left[$i] > $right[$j])
+        {
+            $res[]=$right[$j];
+            $j++;
+        }
+        else
+        {
+            $res[]=$left[$i];
+            $i++;
+        }
+    }
+    while($i<count($left))
+    {
+        $res[]=$left[$i];
+        $i++;    
+    }
+    while($j<count($right))
+    {
+        $res[]=$right[$j];
+            $j++;
+    }
+    return $res;
+}
+?>
