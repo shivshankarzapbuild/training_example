2dfa29861dfa7566d0187909a5b52f1605469b46
shivshankarzapbuild <shivshankar.zapbuild@gmail.com>
2020-02-06 18:04:31 +0530

    Date time exercise modified

--- Exercise_Array/date_time_exercise.php
+++ Exercise_Array/date_time_exercise.php
@@ -7,16 +7,21 @@
 <?php
 
 //to convert date into timestamp
+
     echo "Convert date into timestamp <br> ";
     $date=date_create();
-   // $date=date_create();
+    // $date=date_create();
    // echo date_timestamp_get($date);
     print_r(date_timestamp_set($date));
 
+   
+    
     //echo date_format($date,"y");
     $date=date_create();
     print_r(date_timestamp_set($date));
     echo "<br>".time()."<br>";
+    
+    
     //add 1 months in the current date
     echo "<br>Add 1 month in the current date";
 
@@ -25,6 +30,8 @@
     date_add($date,date_interval_create_from_date_string('1 Month'));
     echo "<br>".date_format($date,"y/m/d");
 
+    
+    
     //  Add 2 days in the current date
     $date = date_create("2020-02-05");
 
@@ -32,6 +39,8 @@
 
     echo "<br>".date_format($date,"y/m/d") ."<br>";
 
+    
+    
     //date difference 
 
     echo "<br>To find the difference between the two dates  2020-06-19 13:44:02 and 2020-10-13 23:04:22";
@@ -44,6 +53,8 @@
 
     echo "<br>".$date->format("%R%a days");
 
+    
+    
     //utc time (time in the current timezone)
 
     echo "<BR><BR>Show time in UTC ()";
@@ -55,6 +66,7 @@
 
    // echo date();
 
+//    Convert timestamp in the date
     echo "<br>Convert timestamp in the date ";
 
    $date = date_create();
@@ -62,6 +74,7 @@
 
    echo "<br>".date_format($date,"U = Y-m-d"). "<br><br>";
 
+//    Convert 2020-06-19 13:44:02 into 19/06/2020 01:44:02 am
 
   echo "<br><br>Convert 2020-06-19 13:44:02 into 19/06/2020 01:44:02 am";
    $date = date_create("2020-06-19 13:44:02");
