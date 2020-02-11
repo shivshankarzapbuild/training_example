36633730a445032c9d6c35e01fa3b5bb9c0c4645
shivshankarzapbuild <shivshankar.zapbuild@gmail.com>
2020-02-11 09:51:46 +0530

    string concatanation character by character

---
+++ php_session/concatanateString.php
@@ -0,0 +1,32 @@
+<?php
+    $str1 = "ZAPBUILD";
+    $str2 = "TRAINEE";
+
+    $length = strlen($str1);
+    $length2 = strlen($str2);
+
+    $str3 = array();
+    $i =0;
+    $j=0;
+    // $k =0;
+
+    //  print_r(str_split($str1,1));
+    //  print_r(str_split($str2,1));
+
+     echo $length .  $length;
+    
+
+    for(;$i<=$length,$j<=$length2;$i++,$j++){
+        
+        echo $str1[$i].$str2[$j];
+        // // }elseif($i>$j){
+        //     $str3[$k]=$str2[$j];
+        //     $j++;
+        //     $k++;
+        // }
+    }
+
+
+
+    
+?>
